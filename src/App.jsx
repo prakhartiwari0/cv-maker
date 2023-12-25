2119914fe1cca6a8de6e9a3057df0a1af388c00a
Prakhar Tiwari <65062036+prakhartiwari0@users.noreply.github.com>
2023-12-25 21:13:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+import Editor from './components/Editor'
+import Output from './components/Output'
+
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+    <Editor></Editor>
+    <Output></Output>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Editor.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react';
+
+import '../styles/editor.css'
+
+
+export default function Editor(){
+
+
+    return (
+        <>
+        <h1>Editor</h1>
+
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" />
+        
+        
+        </>
+    )
+}
